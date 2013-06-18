a41aa0fc19fa549c620d46b645da0cf9adf2ed5c
Fletcher Nichol <fnichol@nichol.ca>
2013-06-17 22:01:08 -0600

    Support chef-solo and/or reading users from an attribute.
    A new attribute (`node['znc']['users']`) will be used if the cookbook is
    converged with Chef Solo or if the attribute is set to a truthy value
    such as an Array. Otherwise converging with Chef Client will still use
    the search by default preserving previous behavior.

--- attributes/default.rb
+++ attributes/default.rb
@@ -32,6 +32,7 @@ default['znc']['conf_dir']        = "#{znc['data_dir']}/configs"
 default['znc']['log_dir']         = "#{znc['data_dir']}/moddata/adminlog"
 default['znc']['module_dir']      = "#{znc['data_dir']}/modules"
 default['znc']['users_dir']       = "#{znc['data_dir']}/users"
+default['znc']['users']           = Chef::Config[:solo] ? [] : nil
 
 default['znc']['port']            = "+7777"
 default['znc']['skin']            = "dark-clouds"
