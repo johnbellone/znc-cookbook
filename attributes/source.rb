91d470707057c6f7bc4f23969ee475d21bcc40ee
BK Box <bk.box@rackspace.com>
2013-07-16 13:56:46 -0500

    Source no longer works on 0.098, update to latest

--- attributes/source.rb
+++ attributes/source.rb
@@ -18,6 +18,6 @@
 #
 
 default['znc']['url'] = "http://znc.in/releases/archive"
-default['znc']['version'] = "0.098"
+default['znc']['version'] = "latest"
 default['znc']['checksum'] = "3b88d33c21e464aa82c84b2dc3bcd52dec95c87a052bb80aff6336dbb4043eb4"
 default['znc']['configure_options'] = %W{ --enable-extra }
