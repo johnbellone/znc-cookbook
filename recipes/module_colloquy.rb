e2d9cb9187107db89850928bbdb6af051e6eac1d
John Bellone <john.bellone.jr@gmail.com>
2014-11-20 07:17:19 -0500

    Fix a few copyright tidbits.

--- recipes/module_colloquy.rb
+++ recipes/module_colloquy.rb
@@ -3,7 +3,7 @@
 # Recipe:: module_colloquy
 #
 # Copyright (c) 2011-2013, Seth Chisamore
-# Copyright (c) 2014, John Bellone <jbellone@bloomberg.net>
+# Copyright (c) 2014, John Bellone
 # Copyright (c) 2014, Bloomberg Finance L.P.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
